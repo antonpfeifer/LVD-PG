1f3c70ca1f3cd89abb6bd96732390adfb296d53b
Anton Pfeifer <anton.pfeifer@gmx.net>
2026-05-16 14:20:07 +0200

    fix: progressive growing script runs without multiple gpus

--- exps/progressive_growing/pg.sh
+++ exps/progressive_growing/pg.sh
@@ -1,8 +1,9 @@
+julia_project_location="../../"
 #do global clustering and log firstly
-CUDA_VISIBLE_DEVICES=2 julia parallel_PG.jl 1 1 400 "${@}"
+CUDA_VISIBLE_DEVICES=0 julia --project="${julia_project_location}" parallel_PG.jl 1 1 400 "${@}"
 #run multi-process to do progressive growing
-CUDA_VISIBLE_DEVICES=2 julia parallel_PG.jl 1 100 400 "${@}" &
-CUDA_VISIBLE_DEVICES=3 julia parallel_PG.jl 101 200 400 "${@}" &
-CUDA_VISIBLE_DEVICES=4 julia parallel_PG.jl 201 300 400 "${@}" &
-CUDA_VISIBLE_DEVICES=5 julia parallel_PG.jl 301 400 400 "${@}" &
+CUDA_VISIBLE_DEVICES=0 julia --project="${julia_project_location}" parallel_PG.jl 1 100 400 "${@}" &
+CUDA_VISIBLE_DEVICES=0 julia --project="${julia_project_location}" parallel_PG.jl 101 200 400 "${@}" &
+CUDA_VISIBLE_DEVICES=0 julia --project="${julia_project_location}" parallel_PG.jl 201 300 400 "${@}" &
+CUDA_VISIBLE_DEVICES=0 julia --project="${julia_project_location}" parallel_PG.jl 301 400 400 "${@}" &
 wait
