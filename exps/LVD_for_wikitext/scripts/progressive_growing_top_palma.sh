cceb8dd01ceab72ab584e28f49fe7f31818c9633
Anton Pfeifer <mpfeife3@uni-muenster.de>
2026-06-30 21:45:05 +0200

    feat: created wikitext top level palma script

---
+++ exps/LVD_for_wikitext/scripts/progressive_growing_top_palma.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --partition=gpuh200mini
+#SBATCH --gres=gpu:1
+#SBATCH --mem=300G
+
+#SBATCH --job-name=wikitext_top_level_train
+#SBATCH --output=wikitext_top_level_300626.dat
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=anton.pfeifer@uni-muenster.de
+
+set -a
+source ../../.env
+set +a
+
+source /scratch/tmp/mpfeife3/bachelorarbeit/miniconda/etc/profile.d/conda.sh
+conda activate lvd-pg
+
+ml palma/2024a
+ml GCC/13.3.0
+ml CUDA/13.0.2
+
+srun python ../progressive_growing_top.py
