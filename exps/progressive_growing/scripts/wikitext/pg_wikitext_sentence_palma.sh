6af966688e46e7c3f719d1f67e0ee9446d06acb2
Anton Pfeifer <anton.pfeifer@gmx.net>
2026-09-12 12:36:34 +0200

    fix: added wikitext sentence pg running scripts

---
+++ exps/progressive_growing/scripts/wikitext/pg_wikitext_sentence_palma.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+BASE_PATH="/scratch/tmp/mpfeife3/bachelorarbeit/lvd-pg/exps/progressive_growing"
+
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+
+#SBATCH --job-name=wikitext_sentence
+#SBATCH --output="${BASE_PATH}/temp/logs/pg_wikitext_070726.dat"
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=anton.pfeifer@uni-muenster.de
+
+#SBATCH --mem=128G
+#SBATCH --partition=gpuh200mini
+#SBATCH --gres=gpu:1
+
+srun bash pg_wikitext_sentence.sh
