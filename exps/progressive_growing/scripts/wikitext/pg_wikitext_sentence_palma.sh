f7e5129bb290f4d7c0537d2d61477a10f2ade55c
Anton Pfeifer <mpfeife3@uni-muenster.de>
2026-09-13 15:02:55 +0200

    fix: shell script code before palma comments prevented them from being used properly

--- exps/progressive_growing/scripts/wikitext/pg_wikitext_sentence_palma.sh
+++ exps/progressive_growing/scripts/wikitext/pg_wikitext_sentence_palma.sh
@@ -1,17 +1,17 @@
-#!/bin/sh
-
-BASE_PATH="/scratch/tmp/mpfeife3/bachelorarbeit/lvd-pg/exps/progressive_growing"
+#!/bin/bash
 
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
 
 #SBATCH --job-name=wikitext_sentence
-#SBATCH --output="${BASE_PATH}/temp/logs/pg_wikitext_070726.dat"
+#SBATCH --output=/scratch/tmp/mpfeife3/bachelorarbeit/lvd-pg/exps/progressive_growing/temp/logs/pg_wikitext_070726.dat
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=anton.pfeifer@uni-muenster.de
 
 #SBATCH --mem=256G
 #SBATCH --partition=gpuh200mini
 #SBATCH --gres=gpu:1
+
+BASE_PATH="/scratch/tmp/mpfeife3/bachelorarbeit/lvd-pg/exps/progressive_growing"
 
 srun bash pg_wikitext_sentence.sh
